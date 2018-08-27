41f3940f08d850608c42b1e70d83ad5097926a9e
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-08-27 18:18:22 +0530

    Display the result

--- js/main.js
+++ js/main.js
@@ -4,14 +4,6 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -20,6 +12,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     dice = Math.floor(Math.random() * 6) + 1;
     
     // 2. Display the result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'img/dice-' + dice + '.png';
     
     // 3. Update the round score IF the rolled number was NOT a 1
 });
